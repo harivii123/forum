988ca965d9b2d2cb608a8550343616e0ac67f9ae
Ville Matti Aleksi Ainamo <k-vainamo@c1r5p5.hive.fi>
2026-09-07 17:48:55 +0300

    tables

---
+++ createtables.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+func CreateTables(db *sql.DB) {
+
+	_, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS user (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT NOT NULL,
+		email TEXT NOT NULL,
+		password TEXT NOT NULL,
+		profile_picture BLOB
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS post (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		body TEXT NOT NULL,
+		image BLOB
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS like (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		like_value INTEGER NOT NULL
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS comment (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		body TEXT NOT NULL
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS user_like (
+		user_id INTEGER NOT NULL,
+		like_id INTEGER NOT NULL,
+		PRIMARY KEY (user_id, like_id),
+		FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+		FOREIGN KEY (like_id) REFERENCES like(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS user_comment (
+		user_id INTEGER NOT NULL,
+		comment_id INTEGER NOT NULL,
+		PRIMARY KEY (user_id, comment_id),
+		FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+		FOREIGN KEY (comment_id) REFERENCES comment(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS user_post (
+		user_id INTEGER NOT NULL,
+		post_id INTEGER NOT NULL,
+		PRIMARY KEY (user_id, post_id),
+		FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+		FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS comment_like (
+		comment_id INTEGER NOT NULL,
+		like_id INTEGER NOT NULL,
+		PRIMARY KEY (comment_id, like_id),
+		FOREIGN KEY (comment_id) REFERENCES comment(id) ON DELETE CASCADE,
+		FOREIGN KEY (like_id) REFERENCES like(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS post_comment (
+		post_id INTEGER NOT NULL,
+		comment_id INTEGER NOT NULL,
+		PRIMARY KEY (post_id, comment_id),
+		FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+		FOREIGN KEY (comment_id) REFERENCES comment(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS post_like (
+		post_id INTEGER NOT NULL,
+		like_id INTEGER NOT NULL,
+		PRIMARY KEY (post_id, like_id),
+		FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+		FOREIGN KEY (like_id) REFERENCES like(id) ON DELETE CASCADE
+		);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
