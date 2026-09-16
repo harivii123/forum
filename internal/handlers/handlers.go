ea7c706c638623d431c7d1f9deb6401e6bd07fec
Ville Matti Aleksi Ainamo <k-vainamo@c1r5p15.hive.fi>
2026-09-16 17:25:32 +0300

    added fts, virtual tables, a helper function for homepage, mainSearch in /database, a little html tomfoolery

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"forum/internal/database"
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func (h *Holder) LoadRegistryPage(w http.ResponseWriter, r *http.Request) {
 	// check stuff insert stuff bang
@@ -8,22 +14,27 @@ func (h *Holder) LoadRegistryPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Holder) LoadFrontPage(w http.ResponseWriter, r *http.Request) {
-		//parse error html
-		// status no bueno 
-		// return
-	
-	//parse html
-	// get css
-	// get info from db
-	//build page
-	// query handling 
-	// formvalue 
-	// all good stuff
-	// 
+	t, err := template.ParseFiles("./internal/templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	mainSearchValue := r.URL.Query().Get("search")
+	log.Println(mainSearchValue)
+	match, err := h.MainSearch(mainSearchValue)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println(match)
+
+	w.WriteHeader(http.StatusOK)
+	err = t.Execute(w, match)
 }
 
 func (h *Holder) LoadPostPage(w http.ResponseWriter, r *http.Request) {
-	//get sttuff from url/body 
+	//get sttuff from url/body
 	// go to repo get data from there and build page with data
 }
 
@@ -32,5 +43,17 @@ func (h *Holder) LoadProfilePage(w http.ResponseWriter, r *http.Request) {
 	// can be maybe also used to checkout other profiles not just ur own?
 }
 
-// func (h *Holder) ToBeContinuedMaybe(?){
-// } 
+// takes value from main search bar and finds all matches using fts(fast text search)
+func (h *Holder) MainSearch(mainSearchValue string) (match []database.SearchResult, err error) {
+
+	if mainSearchValue == "" {
+		return nil, nil
+	}
+	match, err = database.FindAMatch(h.db, mainSearchValue)
+	if err != nil {
+		return nil, nil
+	}
+
+	fmt.Println(match) //template.Execute(w, match)
+	return match, nil
+}
