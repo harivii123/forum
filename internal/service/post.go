b7bb97ae59fdd3a81213f7b8713a413b2417fd2d
Vivian Stolt <132815629+VivianStolt@users.noreply.github.com>
2026-09-23 21:07:38 +0300

    Chores

--- internal/service/post.go
+++ internal/service/post.go
@@ -3,10 +3,13 @@ package service
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"forum/internal/database"
 	"forum/internal/models"
+	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // takes value from main search bar and finds all matches using fts(fast text search)
@@ -124,4 +127,34 @@ func postCats(match string) []models.CategoryView {
 		categories = append(categories, category)
 	}
 	return categories
+}
+
+func CreatePost(cookie *http.Cookie, newPost models.Post, db *sql.DB) (error, int) {
+
+	if newPost.Body == "" || newPost.Title == "" {
+		return errors.New("Empty post"), http.StatusBadRequest // do nothing
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	defer tx.Rollback()
+
+	userID, err := database.GetUserIDWithToken(tx, cookie.Value)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	newPost.UserID, newPost.Created = userID, time.Now()
+
+	err = database.CreatePost(tx, newPost)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+
+	return nil, http.StatusSeeOther
 }
